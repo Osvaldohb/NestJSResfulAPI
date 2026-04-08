1e027742c19c85ab3ea03ecc6bcf1a93340047e5
Osvaldo Santillan <osvaldonuevo@gmail.com>
2026-04-08 14:44:04 -0600

    feat: implement pagination for Pokémon retrieval and enhance validation

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v2');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions:{
+        enableImplicitConversion: true
+      }
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
